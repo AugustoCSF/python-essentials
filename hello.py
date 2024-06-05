ef01f834af81c549db682de7e0402d73d8c8bca5
Augusto Ferreira <guceferreira@gmail.com>
2024-06-05 18:43:23 -0400

    Implementação do hello world multi lingua

--- hello.py
+++ hello.py
@@ -1,4 +1,29 @@
 # esse programa printa HELLO WORLD!
 
-hello = "hello world!"
-print(hello.upper())
+"""Hello world multi_línguas
+
+Este programa imprime HELLO WORLD de acordo com a língua do ambiente.
+
+Como usar:
+    Tenha a variavel LANG devidamente configurada.
+
+    use:
+        python hello.py
+"""
+
+__version__ = 0;0;1
+__author__ = "Augusto Ferreira"
+__license__ = "Unlicense"
+
+import os
+
+current_lang = os.getenv("LANG", "en_US")[:5]
+
+default = "Hello, World!"
+
+if current_lang == "pt_BR":
+    print("Olá, Mundo!")
+elif current_lang == "it_IT":
+    print("Ciao, Mondo!")
+else: 
+    print(default)
