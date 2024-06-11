7e2db5aef623b06a5c3e77375d725965f8ca3640
Augusto Ferreira <guceferreira@gmail.com>
2024-06-10 21:23:14 -0400

    Versão atualizada da tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,10 +1,16 @@
 """Este programa imprime a tabuada do 1 ao 10"""
-__version__ = 0;0;1
+__version__ = 0;1;1
 __author__ = "Augusto Ferreira"
 
 numeros = list(range(1, 11))
+largura = 20
 
 for numero in numeros:
-    print(f"Tabuada do {numero}:")
+    title = f"Tabuada do {numero}".center(largura, "-")
+    print()
+    print(title)
+    print()
     for n in numeros:
-        print(f"{numero} x {n} = ", numero * n)
+        print(f"{numero} x {n} = {numero * n}".center(largura))
+    print()
+    print("_" * largura)
