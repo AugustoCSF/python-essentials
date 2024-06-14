2dd5864e17214c8b6c0881763868d40ee13b1612
Augusto Ferreira <guceferreira@gmail.com>
2024-06-14 16:53:48 -0400

    Implementando com uso de dicionarios

--- escola_v2.py
+++ escola_v2.py
@@ -7,22 +7,25 @@ que frequentas cada uma das atividades.
 __version__ = "0.2.0"
 
 # Dados
-sala1 = ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"]
-sala2 = ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+salas = {
+    "sala1": ["Erik", "Maia", "Gustavo", "Manuel", "Sofia", "Joana"],
+    "sala2": ["Joao", "Antonio", "Carlos", "Maria", "Isolda"]
+}
 
-aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
-aula_musica = ["Erik", "Carlos", "Maria"]
-aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
+atividades = {
+    "Inglês": ["Erik", "Maia", "Joana", "Carlos", "Antonio"],
+    "Música": ["Erik", "Carlos", "Maria"],
+    "Dança": ["Gustavo", "Sofia", "Joana", "Antonio"]
+}
 
 # Listar alunos em cada atividade por sala
-
-atividades = [("Inglês", aula_ingles), ("Música", aula_musica), ("Dança", aula_danca)]
-
-for atividade, aula in atividades:
-    turma_1 = set(sala1) & set(aula)
-    turma_2 = set(sala2) & set(aula)
+for atividade, alunos in atividades.items():
+    turma_por_sala = {
+        "sala1": set(salas["sala1"]) & set(alunos),
+        "sala2": set(salas["sala2"]) & set(alunos)
+    }
 
     print(f"Alunos de {atividade}")
-    print("Sala 1", turma_1)
-    print("Sala 2", turma_2)
+    for sala, turma in turma_por_sala.items():
+        print(f"{sala.capitalize()}: {sorted(turma)}")
     print("-" * 25)
